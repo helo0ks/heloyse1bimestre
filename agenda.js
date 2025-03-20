42cae0ca1d664ef17eb714ee4ab71fea95e29e19
helo0ks <tsukk1hell@gmail.com>
2025-03-20 00:05:17 -0300

    terceiro commit

---
+++ agenda.js
@@ -0,0 +1,10 @@
+class Agenda {
+    constructor(id, materia, professor, atividade, entrega, posicaolista) {
+        this.id = id;
+        this.materia = materia;
+        this.professor = professor;
+        this.atividade= atividade;
+        this.entrega = entrega;
+        this.posicaolista = posicaolista;
+    }
+}
